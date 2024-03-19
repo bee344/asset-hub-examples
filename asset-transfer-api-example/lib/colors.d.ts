552048393b4eebcbaeea617d1a15eb7b372edcc6
bee344 <alberto.penayo@parity.io>
2024-03-19 18:33:28 -0300

    .gitignore Fixed

--- asset-transfer-api-example/lib/colors.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const GREEN = "\u001B[32m";
-export declare const CYAN = "\u001B[36m";
-export declare const PURPLE = "\u001B[35m";
-export declare const RESET = "\u001B[0m";
--- asset-transfer-api-example/lib/colors.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RESET = exports.PURPLE = exports.CYAN = exports.GREEN = void 0;
-exports.GREEN = '\u001b[32m';
-exports.CYAN = '\u001b[36m';
-exports.PURPLE = '\u001b[35m';
-exports.RESET = '\u001b[0m';
-//# sourceMappingURL=colors.js.map
